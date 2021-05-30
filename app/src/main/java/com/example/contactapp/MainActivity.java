cfd70717c8c1ac9979953db8ca2468a142bc9485
Ayesha Momina <mcsf19m022@pucit.edu.pk>
2021-05-30 13:46:00 +0500

    Array adapter applied and fetching data from database through cursor

--- app/src/main/java/com/example/contactapp/MainActivity.java
+++ app/src/main/java/com/example/contactapp/MainActivity.java
@@ -3,9 +3,16 @@ package com.example.contactapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +22,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        MyHelper helper = new MyHelper(this);
+        SQLiteDatabase db = helper.getReadableDatabase();
+        Cursor cursor = db.rawQuery("select * from contact",new String[]{});
+
+        listView = findViewById(R.id.ListView);
+
+        if (cursor.moveToNext() != false){
+            ArrayList<String> contactList = new ArrayList<String>();
+            do {
+                contactList.add(cursor.getString(1));
+            }while (cursor.moveToNext());
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                    this, android.R.layout.simple_list_item_1,contactList
+            );
+            listView.setAdapter(adapter);
+
+
+        }
 
     }
 
