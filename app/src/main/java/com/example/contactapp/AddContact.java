91259afa93cf49ddd3dd63a556ca998fd5dd08b8
Ayesha Momina <mcsf19m022@pucit.edu.pk>
2021-05-30 13:52:09 +0500

    edit facility applied for xml when edit a contact

--- app/src/main/java/com/example/contactapp/AddContact.java
+++ app/src/main/java/com/example/contactapp/AddContact.java
@@ -3,6 +3,7 @@ package com.example.contactapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +18,25 @@ public class AddContact extends AppCompatActivity {
         setContentView(R.layout.activity_add_contact);
         Intent intent = getIntent();
         String id =intent.getStringExtra("id");
+
+        if (id != null){
+            name = findViewById(R.id.editTextTextPersonName);
+            phone = findViewById(R.id.editTextTextPersonName1);
+            company = findViewById(R.id.editTextTextPersonName3);
+            email = findViewById(R.id.editTextTextPersonName4);
+
+            MyHelper helper = new MyHelper(this);
+            SQLiteDatabase db = helper.getWritableDatabase();
+            Cursor cursor = db.rawQuery("select * from contact",new String[]{});
+            cursor.moveToFirst();
+            for (int i = 2;i <= Integer.valueOf(id); i++){
+                cursor.moveToNext();
+            }
+            name.setText(cursor.getString(1));
+            phone.setText(cursor.getString(2));
+            company.setText(cursor.getString(3));
+            email.setText(cursor.getString(4));
+        }
     }
 
     public void save(View view) {
