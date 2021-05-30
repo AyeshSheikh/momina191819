a8a656d905694807552307356ef676364e687dbf
Ayesha Momina <mcsf19m022@pucit.edu.pk>
2021-05-30 13:41:06 +0500

    Contact Details View Activity designed

--- app/src/main/java/com/example/contactapp/AddContact.java
+++ app/src/main/java/com/example/contactapp/AddContact.java
@@ -3,6 +3,7 @@ package com.example.contactapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -23,5 +24,19 @@ public class AddContact extends AppCompatActivity {
         phone = findViewById(R.id.editTextTextPersonName1);
         company = findViewById(R.id.editTextTextPersonName3);
         email = findViewById(R.id.editTextTextPersonName4);
+
+        ContactModel contactDetails = new ContactModel(
+                name.getText().toString(),phone.getText().toString(),company.getText().toString(),email.getText().toString()
+        );
+        MyHelper helper = new MyHelper(this);
+        SQLiteDatabase database = helper.getWritableDatabase();
+
+        Intent intent = getIntent();
+        String id = intent.getStringExtra("id");
+
+        helper.insertData(database,contactDetails,id);
+
+        Intent intent1 = new Intent(this,MainActivity.class);
+        startActivity(intent1);
     }
 }
