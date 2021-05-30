8d05912604e5664ca72c55f4f2a4d61191d6f777
Ayesha Momina <mcsf19m022@pucit.edu.pk>
2021-05-30 13:32:21 +0500

    Contact Model Class created and Implemented

--- app/src/main/java/com/example/contactapp/AddContact.java
+++ app/src/main/java/com/example/contactapp/AddContact.java
@@ -2,17 +2,26 @@ package com.example.contactapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class AddContact extends AppCompatActivity {
 
+    EditText name,phone,company,email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contact);
+        Intent intent = getIntent();
+        String id =intent.getStringExtra("id");
     }
 
     public void save(View view) {
+        name = findViewById(R.id.editTextTextPersonName);
+        phone = findViewById(R.id.editTextTextPersonName1);
+        company = findViewById(R.id.editTextTextPersonName3);
+        email = findViewById(R.id.editTextTextPersonName4);
     }
 }
---
+++ app/src/main/java/com/example/contactapp/ContactModel.java
@@ -0,0 +1,57 @@
+package com.example.contactapp;
+
+public class ContactModel {
+    private String name;
+    private String phone;
+    private String company;
+    private String email;
+
+    public ContactModel(String name, String phone, String company, String email) {
+        this.name = name;
+        this.phone = phone;
+        this.company = company;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "ContactModel{" +
+                "name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", company='" + company + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
